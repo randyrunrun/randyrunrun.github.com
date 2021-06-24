702baa39037824ea8c9aa0ad54c1ca868f05e29b
zhouchanghao <zhouchanghao@meituan.com>
2021-06-24 13:09:33 +0800

    编辑器

---
+++ src/components/App.js
@@ -0,0 +1,43 @@
+import React, { PureComponent } from 'react';
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Header from './Header';
+import PostListContainer from './PostListContainer';
+import PostContainer from './PostContainer';
+import Editer from './Editer';
+import store from '../redux/store';
+
+export default class App extends PureComponent {
+  _redirectToHome() {
+    return <Redirect to="/" />;
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <MuiThemeProvider>
+          <Router>
+            <div>
+              <Header />
+
+              {/* content */}
+              <Switch>
+                <Route exact path="/" component={PostListContainer} />
+                <Route path="/posts/:id/:slug" component={PostContainer} />
+                <Route exact path="/edit" component={Editer} />
+                {/* catch-all redirects to home */}
+                <Route render={this._redirectToHome} />
+              </Switch>
+            </div>
+          </Router>
+        </MuiThemeProvider>
+      </Provider>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/App';
+var COS = require('cos-js-sdk-v5');
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +10,27 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+var cos = new COS({
+  SecretId: 'AKIDGUYXuBcH5lGdM748OVYSAnX8ZO2TN07B',
+  SecretKey: '2tY1LVYRqDiCYkneLuJ9oKtbKwPkPtFW',
+});
+
+cos.putObject({
+  Bucket: 'reading-1259170587', /* 必须 */
+  Region: 'ap-chengdu',     /* 存储桶所在地域，必须字段 */
+  Key: 'post/postClass',              /* 必须 */
+  Body: JSON.stringify({a:1,b:2}), // 上传文件对象
+  onProgress: function(progressData) {
+      console.log(JSON.stringify(progressData));
+  }
+}, function(err, data) {
+  console.log(err || data);
+});
+
+cos.getObject({
+  Bucket: 'reading-1259170587', /* 必须 */
+  Region: 'ap-chengdu',     /* 存储桶所在地域，必须字段 */
+  Key: 'post/postClass',           /* 非必须 */
+}, function(err, data) {
+  console.log(err || data.Body);
+});
